d592cb8a20bd74c08a58b3c949992c4f9e0ee0dc
zerodott <93039953+zerodott@users.noreply.github.com>
2022-03-22 09:37:59 +0200

    add slider to home page

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,36 @@ let slider = $(document).ready(function () {
   });
 });
 
+let slider_logo = $(document).ready(function () {
+  $('.partners__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    variableWidth: true,
+    focusOnSelect: true,
+    autoplay: true,
+    centerMode: true,
+    autoplaySpeed: 3500
+    
+  });
+});
+
+let slider_team = $(document).ready(function () {
+  $('.team__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    centerMode: true,
+    arrows: false,
+    variableWidth: true,
+    focusOnSelect: true,
+    infinite: true
+  });
+});
+
+
 $(document).ready(function(){
   $('.header__burger').click(function(event){
       $('.header__burger, .header__info').toggleClass('toggled');

0833e571bdaabcb67e1c787ef39c76fcd5f8df5d
zerodott <93039953+zerodott@users.noreply.github.com>
2022-03-26 08:10:10 +0200

    Add check mobile or desktop in js

--- src/js/script.js
+++ src/js/script.js
@@ -20,7 +20,7 @@ let slider_logo = $(document).ready(function () {
     autoplay: true,
     centerMode: true,
     autoplaySpeed: 3500
-    
+
   });
 });
 
@@ -37,9 +37,25 @@ let slider_team = $(document).ready(function () {
 });
 
 
-$(document).ready(function(){
-  $('.header__burger').click(function(event){
-      $('.header__burger, .header__info').toggleClass('toggled');
+$(document).ready(function () {
+  $('.header__burger').click(function (event) {
+    $('.header__burger, .header__info').toggleClass('toggled');
   });
 });
 
+let isMobile = {
+  Android: function () { return navigator.userAgent.match(/Android/i); },
+  BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
+  iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+  Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
+  Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
+  any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
+};
+
+let body = document.querySelector('body');
+
+if(isMobile.any()){
+  body.classList.add('touch');
+}else{
+  body.classList.add('mouse');
+}
